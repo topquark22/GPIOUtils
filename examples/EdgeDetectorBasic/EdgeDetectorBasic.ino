ebc1186daea3f607012d7c659ce956c39d2437a0
Geoffrey Falk <topquark22@yahoo.ca>
2026-01-06 02:15:29 -0500

    output also to LED on pin 5

--- examples/EdgeDetectorBasic/EdgeDetectorBasic.ino
+++ examples/EdgeDetectorBasic/EdgeDetectorBasic.ino
@@ -1,10 +1,14 @@
 #include <gpioutils.h>
 
+// assume clean input (no debounce)
 const uint8_t PIN_IN = 3;
+
+const uint8_t PIN_LED = 5;
 
 EdgeDetector ed(PIN_IN, INPUT_PULLUP);
 
 void setup() {
+  pinMode(PIN_LED, OUTPUT);
   Serial.begin(115200);
   while (!Serial) {}
   ed.begin();
@@ -12,6 +16,12 @@ void setup() {
 
 void loop() {
   ed.read();
-  if (ed.rose()) Serial.println("Rising edge");
-  if (ed.fell()) Serial.println("Falling edge");
+  if (ed.rose()) {
+    Serial.println("Rising edge");
+    digitalWrite(PIN_LED, HIGH);
+  }
+  if (ed.fell()) {
+    Serial.println("Falling edge");
+    digitalWrite(PIN_LED, LOW);
+  }
 }
