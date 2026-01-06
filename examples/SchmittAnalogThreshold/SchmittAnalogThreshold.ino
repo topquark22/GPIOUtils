5b781b7f14e5a642ce9c00897c1f1a5d035f901a
Geoffrey Falk <topquark22@yahoo.ca>
2026-01-06 01:38:17 -0500

    add LED output

--- examples/SchmittAnalogThreshold/SchmittAnalogThreshold.ino
+++ examples/SchmittAnalogThreshold/SchmittAnalogThreshold.ino
@@ -1,13 +1,18 @@
 #include <gpioutils.h>
 
+const uint8_t PIN_LED = 5;
+
 Schmitt sch(A0, 480, 540);
 
 void setup() {
+  pinMode(PIN_LED, OUTPUT);
   Serial.begin(115200);
   while (!Serial) {}
   sch.begin();
 }
 
 void loop() {
-  Serial.println(sch.read() ? "HIGH" : "LOW");
+  bool v = sch.read();
+  digitalWrite(PIN_LED, v);
+  Serial.println(v ? "HIGH" : "LOW");
 }
