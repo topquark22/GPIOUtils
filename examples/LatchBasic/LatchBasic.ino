5a4b4e65c72213aadf31da9732076faeba7b62a3
Geoffrey T. Falk <topquark22@yahoo.ca>
2026-01-08 17:58:09 -0500

    start debouncers

--- examples/LatchBasic/LatchBasic.ino
+++ examples/LatchBasic/LatchBasic.ino
@@ -7,12 +7,12 @@
     - SET button on pin 2 (to GND, INPUT_PULLUP)
     - RESET button on pin 3 (to GND, INPUT_PULLUP)
 
-  Latch drives LED on pin 13.
+  Latch drives LED on pin 5.
 */
 
 static const uint8_t PIN_SET = 2;
 static const uint8_t PIN_RST = 3;
-static const uint8_t PIN_LED = 13;
+static const uint8_t PIN_LED = 5;
 
 Debounce setBtn(PIN_SET, INPUT_PULLUP, 25);
 Debounce rstBtn(PIN_RST, INPUT_PULLUP, 25);
@@ -21,13 +21,16 @@ Latch latch(false);
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
+  setBtn.begin();
+  rstBtn.begin();
+  Serial.begin(115200); // DEBUG
 }
 
 void loop() {
   setBtn.read();
   rstBtn.read();
 
-  if (setBtn.fell()) latch.set();
+  if (setBtn.fell()) latch.set(); 
   if (rstBtn.fell()) latch.reset();
 
   digitalWrite(PIN_LED, latch.value() ? HIGH : LOW);
