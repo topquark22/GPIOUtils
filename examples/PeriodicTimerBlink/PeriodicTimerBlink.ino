52588c4618c12e6da02b8dd994bca71081e2503b
Geoffrey T. Falk <topquark22@yahoo.ca>
2026-01-09 17:23:06 -0500

    fix output pin

--- examples/PeriodicTimerBlink/PeriodicTimerBlink.ino
+++ examples/PeriodicTimerBlink/PeriodicTimerBlink.ino
@@ -1,16 +1,18 @@
 #include <GPIOUtils.h>
 
+constexpr uint8_t LED_PIN = 2;
+
 constexpr uint32_t PERIOD_MS = 500;
 
 PeriodicTimer blink(PERIOD_MS);
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   blink.start();
 }
 
 void loop() {
   if (blink.tick()) {
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
   }
 }
